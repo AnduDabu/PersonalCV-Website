bde19f0c18f67d7a97afc8ab8836521306f43840
Andu Dabu <andu@Andus-MacBook-Air.local>
2026-01-15 22:26:18 +0200

    feat: add open graph social preview image

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -8,8 +8,13 @@ const SEO = ({ title, description }) => {
             <meta name="description" content={description} />
             <meta property="og:title" content={`${title} | Alexandru Dabu`} />
             <meta property="og:description" content={description} />
+            <meta property="og:image" content="https://alexandrudabu.com/og-image.png" />
+            <meta property="og:type" content="website" />
+
+            <meta property="twitter:card" content="summary_large_image" />
             <meta property="twitter:title" content={`${title} | Alexandru Dabu`} />
             <meta property="twitter:description" content={description} />
+            <meta property="twitter:image" content="https://alexandrudabu.com/og-image.png" />
         </Helmet>
     );
 };
